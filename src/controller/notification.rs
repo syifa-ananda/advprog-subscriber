3314089ae80d5a4e1c630e79f0777abaf730d676
syifa-ananda <syifaananda36@gmail.com>
2025-03-27 01:13:48 +0700

    Implement unsubscribe function in Notification controller.

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -11,4 +11,12 @@ use rocket::serde::json::Json;
          Ok(f) => Ok(Json::from(f)),
          Err(e) => Err(e)
      };
+}
+ 
+    #[get("/unsubscribe/<product_type>")]
+    pub fn unsubscribe(product_type: &str) -> Result<Json<SubscriberRequest>> {
+        return match NotificationService::unsubscribe(product_type) {
+            Ok(f) => Ok(Json::from(f)),
+            Err(e) => Err(e)
+        };
  }
